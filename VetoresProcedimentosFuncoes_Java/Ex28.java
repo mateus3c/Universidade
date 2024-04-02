a24ca7da85b2b951cbedd08646532c003cb1b1f5
mateus3c <122059128+mateus3c@users.noreply.github.com>
2024-04-02 18:02:55 -0300

    Release

---
+++ VetoresProcedimentosFuncoes_Java/Ex28.java
@@ -0,0 +1,18 @@
+/*
+Uma pequena loja de artesanato possui apenas um vendedor e comercializa dez tipos de objetos. O vendedor recebe, mensalmente, salário de R$545,00, acrescido de 5% do valor total de suas vendas. O valor unitário dos objetos deve ser informado e armazenado em um vetor; a quantidade vendida de cada peça deve ficar em outro vetor, mas na mesma posição. Crie um programa que receba os preços e as quantidades vendidas, armazenando-os em seus respectivos vetores (ambos com tamanho dez). Depois, determine e mostre:
+Um relatório contendo: quantidade vendida, valor unitário e valor total de cada objeto. Ao final, deverão ser mostrados o valor geral das vendas e o valor da comissão que será paga ao vendedor; e o valor do objeto mais vendido e sua posição no vetor (não se preocupe com empates).
+*/
+
+import java.util.Scanner;
+
+ class Ex28 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] valoresUnitarios = new int[10];
+        int[] quantidadesVendidas = new int[10];
+        for (i = 0 ; i < 10 ; i++) {
+            
+        }
+
+    }
+}
