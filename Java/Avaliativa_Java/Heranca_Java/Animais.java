5823c46f382258a4530664510b64ff3bc20234d8
mateus3c <122059128+mateus3c@users.noreply.github.com>
2024-04-23 17:45:48 -0300

    Release

--- Java/Avaliativa_Java/Heranca_Java/Animais.java
+++ Java/Avaliativa_Java/Heranca_Java/Animais.java
@@ -1,97 +1,102 @@
 import java.util.Random;
 
+// Superclasse "Animais".
 public class Animais {
+    // Protegeu o pacote "Random", para que o mesmo possa ser usado pela própria superclasse mas também por suas subclasses.
     protected Random rand = new Random();
+    // Atributos que serão compartilhados por ambas subclasses, "Cao" e "Gato".
     public int indexNome;
     public int indexSaude;
     public int idade;
-
+    // Construtor da superclasse "Animais".
     public Animais(String[] arrayNome, String[] arraySaude, int idadeMaxima) {
-        this.indexNome = rand.nextInt(arrayNome.length);
-        this.indexSaude = rand.nextInt(arraySaude.length);
+        this.indexNome = rand.nextInt(arrayNome.length  - 1);
+        this.indexSaude = rand.nextInt(arraySaude.length - 1);
         this.idade = rand.nextInt(idadeMaxima);
-
     }
 
-    // Getter para indexNome
-    public int getIndexNome() {
-        return indexNome;
-    }
-    // Setter para indexNome
+    // Métodos setter da superclasse "Animais".
     public void setIndexNome(int indexNome) {
         this.indexNome = indexNome;
     }
-    // Getter para indexSaude
-    public int getIndexSaude() {
-        return indexSaude;
-    }
-    // Setter para indexSaude
     public void setIndexSaude(int indexSaude) {
         this.indexSaude = indexSaude;
     }
-    // Getter para idade
-    public int getIdade() {
-        return idade;
-    }
-    // Setter para idade
     public void setIdade(int idade) {
         this.idade = idade;
     }
+
+    // Métodos getters da superclasse "Animais".
+    public int getIndexNome() {
+        return indexNome;
+    }
+    public int getIndexSaude() {
+        return indexSaude;
+    }
+    public int getIdade() {
+        return idade;
+    }
 }
 
+
+// Subclasse "Cao" da superclasse "Animais".
 class Cao extends Animais {
+    //Atributos únicos da subclasse "Cao".
     public int indexRacasCao;
     public int indexPortes;
 
+    // Construtor da subclasse "Cao".
     public Cao(String[] arrayNome, String[] arraySaude, int idadeMaxima, String[] arrayRacasCao, String[] arrayPortes) {
-        super(arrayNome, arraySaude, idadeMaxima);
-        this.indexRacasCao = rand.nextInt(arrayRacasCao.length);
-        this.indexPortes = rand.nextInt(arrayPortes.length);
+        super(arrayNome, arraySaude, idadeMaxima); // Apesar de chamar o construtor da superclasse, nesse caso, os atributos da superclasse "Animais" não foram utilizados para nada em específico.
+        this.indexRacasCao = rand.nextInt(arrayRacasCao.length - 1); // Determina que a raça escolhida será aquela que estiver no index sorteado. O index será um número aleatório entre 0 e a quantidade de termos dentro de "arrayRacasCao".
+        this.indexPortes = rand.nextInt(arrayPortes.length - 1); // Determina que o porte escolhida será aquela que estiver no index sorteado. O index será um número aleatório entre 0 e a quantidade de termos dentro de "arrayPortes".
     }
 
-    // Getter para indexRacasCao
-    public int getIndexRacasCao() {
-        return indexRacasCao;
-    }
-    // Setter para indexRacasCao
+    // Métodos setter da superclasse "Animais".
     public void setIndexRacasCao(int indexRacasCao) {
         this.indexRacasCao = indexRacasCao;
     }
-    // Getter para indexPortes
-    public int getIndexPortes() {
-        return indexPortes;
-    }
-    // Setter para indexPortes
     public void setIndexPortes(int indexPortes) {
         this.indexPortes = indexPortes;
+    }
+
+    // Métodos getters da superclasse "Animais".
+    public int getIndexRacasCao() {
+        return indexRacasCao;
+    }
+    public int getIndexPortes() {
+        return indexPortes;
     }
 }
 
 
+// Subclasse "Gato" da superclasse "Animais".
 class Gato extends Animais {
+    // Atributos únicos da subclasse "Gato".
     public int indexRacasGato;
     public int indexPersonalidades;
 
+    // Construtor da subclasse "Gato".
     public Gato(String[] arrayNome, String[] arraySaude, int idadeMaxima, String[] arrayRacasGato, String[] arrayPersonalidades) {
-        super(arrayNome, arraySaude, idadeMaxima);
-        this.indexRacasGato = rand.nextInt(arrayRacasGato.length);
-        this.indexPersonalidades = rand.nextInt(arrayPersonalidades.length);
+        super(arrayNome, arraySaude, idadeMaxima); // Apesar de chamar o construtor da superclasse, nesse caso, os atributos da superclasse "Animais" não foram utilizados para nada em específico.
+        this.indexRacasGato = rand.nextInt(arrayRacasGato.length - 1); // Determina que a raça escolhida será aquela que estiver no index sorteado. O index será um número aleatório entre 0 e a quantidade de termos dentro de "arrayRacasGato".
+        this.indexPersonalidades = rand.nextInt(arrayPersonalidades.length - 1); // Determina que a personalidade escolhida será aquela que estiver no index sorteado. O index será um número aleatório entre 0 e a quantidade de termos dentro de "arrayPersonalidades".
     }
-    // Getter para indexRacasGato
-    public int getIndexRacasGato() {
-        return indexRacasGato;
-    }
-    // Setter para indexRacasGato
+
+    // Métodos setter da superclasse "Animais".
     public void setIndexRacasGato(int indexRacasGato) {
         this.indexRacasGato = indexRacasGato;
     }
-    // Getter para indexPersonalidades
-    public int getIndexPersonalidades() {
-        return indexPersonalidades;
-    }
-    // Setter para indexPersonalidades
     public void setIndexPersonalidades(int indexPersonalidades) {
         this.indexPersonalidades = indexPersonalidades;
+    }
+
+    // Métodos getters da superclasse "Animais".
+    public int getIndexRacasGato() {
+        return indexRacasGato;
+    }
+    public int getIndexPersonalidades() {
+        return indexPersonalidades;
     }
 }
 
--- Java/Avaliativa_Java/Heranca_Java/Main.java
+++ Java/Avaliativa_Java/Heranca_Java/Main.java
@@ -3,26 +3,33 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        // Inicia arrays com várias características dos animais.
         String[] arrayRacasCao = { "Vira-lata", "Salsicha", "Poodle" };
         String[] arrayRacasGato = { "Persa", "Siames", "Sphynx" };
         String[] arrayNome = { "Rex", "Luna", "Bolinha", "Vladmir" };
         String[] arraySaude = { "Saudável", "Doente" };
         String[] arrayPortes = { "Pequeno", "Medio", "Grande" };
         String[] arrayPersonalidades = { "Bravo", "Calmo", "Preguiçoso" };
+        // A idade máxima não é array, mas apenas um valor que servirá como limite para o tamanho dos números que poderão vir no sorteio.
         int idadeMaxima = 15;
 
+        // Loop infinito para que o programa não finalize.
         while(true) {
             System.out.println("Qual animal você deseja?\n[1] Cachorro\n[2] Gato");
             String animal = sc.nextLine();
             if (animal.equals("1")) {
+                // Cria uma instância de "Cao" com o apelido "cao", e chama seus métodos, que pegarão os valores disponíveis na lista de arrays acima.
                 Cao cao = new Cao(arrayNome, arraySaude, idadeMaxima, arrayRacasCao, arrayPortes);
                 System.out.print("\033[H\033[2J");
+                // Significa: "Texto" + "Array[Index do Array]"
                 System.out.println("Raça: " + arrayRacasCao[cao.getIndexRacasCao()]);
+                //O index do array é obtido dentro da classe "Animais", e por isso deve-se chamar o método getter que retorna o valor desejado. Neste caso, o valor desejado é o index de "arrayNome", que é retornado pelo método "getIndexNome" localizado na subclasse "Cao" da superclasse "Animais", por isso é necessário preceder o método getter com o apelido da subclasse.
                 System.out.println("Nome: " + arrayNome[cao.getIndexNome()]);
                 System.out.println("Idade: " + cao.getIdade());
                 System.out.println("Saúde: " + arraySaude[cao.getIndexSaude()]);
                 System.out.println("Porte: " + arrayPortes[cao.getIndexPortes()]);
             } else if (animal.equals("2")) {
+                // Cria uma instância de "Gato" com o apelido "gato", e chama seus métodos, que pegarão os valores disponíveis na lista de arrays acima.
                 Gato gato = new Gato(arrayNome, arraySaude, idadeMaxima, arrayRacasGato, arrayPersonalidades);
                 System.out.print("\033[H\033[2J");
                 System.out.println("Raça: " + arrayRacasGato[gato.getIndexRacasGato()]);
